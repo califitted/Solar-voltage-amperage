8349df98b8ed2c741308b98a2375f653c73e1a7a
zachery nelson <zacherynelson@MacBook.local>
2020-05-14 19:34:34 -0700

    Update Solar.ino
    Adding the load circuit

--- Solar/Solar.ino
+++ Solar/Solar.ino
@@ -12,11 +12,15 @@
 #include <Adafruit_INA219.h>
 
 Adafruit_INA219 ina219;
+Adafruit_INA219 ina219_load(0x41);
 
-// set up the 'temperature' and 'humidity' feeds
+// set up the 'voltage' and 'current' feeds
 AdafruitIO_Feed *voltage = io.feed("solar.solar-panel-voltage");
 AdafruitIO_Feed *power = io.feed("solar.solar-panel-power");
 AdafruitIO_Feed *current = io.feed("solar.solar-panel-current");
+//AdafruitIO_Feed *voltage_load = io.feed("solar.solar-panel-voltage-load");
+AdafruitIO_Feed *power_load = io.feed("solar.solar-panel-power-load");
+AdafruitIO_Feed *current_load = io.feed("solar.solar-panel-current-load");
 
 void setup() {
 
@@ -48,6 +52,10 @@ void setup() {
     Serial.println("Failed to find INA219 chip");
     // while (1) { delay(10); }
   }
+  if (! ina219_load.begin()) {
+    Serial.println("Failed to find INA219 load chip");
+    // while (1) { delay(10); }
+  }
   // To use a slightly lower 32V, 1A range (higher precision on amps):
   //ina219.setCalibration_32V_1A();
   // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
@@ -59,7 +67,8 @@ void setup() {
 void loop() {
 
     // wait 5 seconds (5000 milliseconds == 5 seconds)
-  delay(30000);
+  delay(15000);
+  //  delay(1000);
 
   // io.run(); is required for all sketches.
   // it should always be present at the top of your loop
@@ -74,15 +83,17 @@ void loop() {
   float current_mA = 0;
   float loadvoltage = 0;
   float power_mW = 0;
+  float power_W = 0.0;
 
   shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA();
   power_mW = ina219.getPower_mW();
+  power_W = power_mW/1000.0;
   loadvoltage = busvoltage + (shuntvoltage / 1000);
 
   current->save(current_mA); 
-  power->save(power_mW);
+  power->save(power_W);
   voltage->save(loadvoltage);
   
   Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
@@ -92,6 +103,32 @@ void loop() {
   Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
   Serial.println("");
 
-  delay(2000);
+  float shuntvoltage_load = 0;
+  float busvoltage_load = 0;
+  float current_mA_load = 0;
+  float loadvoltage_load = 0;
+  float power_mW_load = 0;
+  float power_W_load = 0.0;
+
+  shuntvoltage_load = ina219_load.getShuntVoltage_mV();
+  busvoltage_load = ina219_load.getBusVoltage_V();
+  current_mA_load = ina219_load.getCurrent_mA();
+  power_mW_load = ina219_load.getPower_mW();
+  power_W_load = power_mW_load/1000.0;
+  loadvoltage_load = busvoltage_load + (shuntvoltage_load / 1000);
+
+  current_load->save(current_mA_load); 
+  power_load->save(power_W_load);
+//  voltage_load->save(loadvoltage_load);
+  
+  Serial.print("Bus Voltage Load:   "); Serial.print(busvoltage_load); Serial.println(" V");
+  Serial.print("Shunt Voltage Load: "); Serial.print(shuntvoltage_load); Serial.println(" mV");
+  Serial.print("Load Voltage Load:  "); Serial.print(loadvoltage_load); Serial.println(" V");
+  Serial.print("Current Load:       "); Serial.print(current_mA_load); Serial.println(" mA");
+  Serial.print("Power Load:         "); Serial.print(power_mW_load); Serial.println(" mW");
+  Serial.println("");
+
+
+  //delay(2000);
 
 }
